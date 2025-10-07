17fc87e151bce1e74b3024bf2142bdc123a71245
Vorkel <bouches-ecrans4h@icloud.com>
2025-10-07 17:34:54 +0200

    Mise à jour de la version de TensorFlow et ajout de la dépendance Optree dans le script de bootstrap EMR
    - Changement de la version de `tensorflow` dans `bootstrap-emr.sh` de 2.5.0 à 2.15.0 pour bénéficier des dernières améliorations et corrections de bugs.
    - Ajout de la dépendance `optree` pour optimiser les performances des opérations TensorFlow.

--- bootstrap-emr.sh
+++ bootstrap-emr.sh
@@ -7,5 +7,6 @@ sudo python3 -m pip install --upgrade pip setuptools
 # Installation des packages pour le projet
 sudo python3 -m pip install pandas==1.2.5
 sudo python3 -m pip install pillow
-sudo python3 -m pip install tensorflow==2.5.0
+sudo python3 -m pip install tensorflow==2.15.0
 sudo python3 -m pip install pyarrow
+sudo python3 -m pip install optree
